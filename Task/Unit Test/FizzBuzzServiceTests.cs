ad5dfecaf794f03294eea71abcc01f18530bbb6b
Pratik <kuyatepratik8@gmail.com>
2026-02-05 01:46:30 +0530

    Added FizzBuzz unit tests

---
+++ Task/Unit Test/FizzBuzzServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Task.Implement;
+using Xunit;
+
+namespace Task.Unit_Test
+{
+    public class FizzBuzzServiceTests
+    {
+        private readonly FizzBuzzService _service = new FizzBuzzService();
+
+        [Theory]
+        [InlineData("3", "Fizz")]
+        [InlineData("5", "Buzz")]
+        [InlineData("15", "FizzBuzz")]
+        [InlineData("23", "")]
+        [InlineData("A", "Invalid Item")]
+        [InlineData("", "Invalid Item")]
+        public void ProcessValue_ShouldReturnExpectedResult(string input, string expectedOutput)
+        {
+            var result = _service.ProcessValue(input);
+            Assert.Equal(expectedOutput, result.Output);
+        }
+    }
+}
